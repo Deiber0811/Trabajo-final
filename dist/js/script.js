6db402a54d00e7cb35e810bc190f87e777bb1ec5
CarlosAG0 <carlosgonzales2532@gmail.com>
2023-10-03 14:59:47 -0500

    update final final

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,14 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-      require('flowbite/plugin')
-    ]
-  };
- 
 // start: Sidebar
 const sidebarToggle = document.querySelector('.sidebar-toggle')
 const sidebarOverlay = document.querySelector('.sidebar-overlay')
